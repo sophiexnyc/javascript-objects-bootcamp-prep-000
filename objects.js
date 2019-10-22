a451ff2f98429c18f9102b0ba23ce2b5a615be69
Sophie Lee <sblee092@gmail.com>
2019-10-22 15:23:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -10,11 +10,11 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, { [artistName]: songTitle});
 }
 
-updatePlaylist(playlist, "Phil Ochs", "Here's to the State of Mississippi");
+playlist = updatePlaylist(playlist, "Phil Ochs", "Here's to the State of Mississippi");
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
   return playlist;
 }
 
-removeFromPlaylist(playlist, Slowdive);
+playlist = removeFromPlaylist(playlist, Slowdive);
